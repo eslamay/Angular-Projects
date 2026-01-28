1b11b6405008d43923c37353d8b16e18e7b18b2c
Sherlock <playyala@yahoo.com>
2026-01-28 17:58:48 +0200

    add project number 20

--- 20-crypto-charts/src/app/components/crypto-options/crypto-options.component.ts
+++ 20-crypto-charts/src/app/components/crypto-options/crypto-options.component.ts
@@ -1,11 +1,63 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CryptoPriceService } from '../../services/crypto-price.service';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-crypto-options',
-  imports: [],
+  imports: [CommonModule,FormsModule],
   templateUrl: './crypto-options.component.html',
   styleUrl: './crypto-options.component.scss'
 })
-export class CryptoOptionsComponent {
+export class CryptoOptionsComponent implements OnInit {
+  coin: string = '';
+  currency: string = '';
+  days: string = '';
+  coinOptions: string[] = [
+    'bitcoin',
+    'ethereum',
+    'litecoin',
+    'dogecoin',
+    'cardano',
+    'binancecoin',
+    'solana',
+    'polkadot',
+    'ripple',
+    'dogecoin',
+    'uniswap',
+    'chainlink',
+    'shiba-inu',
+    'avalanche',
+    'tron',
+  ];
+  currencyOptions: string[] = [
+    'usd',
+    'eur',
+    'gbp',
+    'jpy',
+    'aud',
+    'cad',
+    'chf',
+    'cny',
+    'inr',
+    'brl',
+  ];
+  daysOptions: string[] = ['7', '14', '30', '90', '180', '365'];
 
+  constructor(private cryptoPriceService: CryptoPriceService) {}
+
+  ngOnInit(): void {
+    this.coin = this.cryptoPriceService.coin;
+    this.currency = this.cryptoPriceService.currency;
+    this.days = this.cryptoPriceService.days;
+  }
+
+  onSubmit() {
+    this.cryptoPriceService.updateCryptoOptions(
+      this.coin,
+      this.currency,
+      this.days
+    );
+  }
 }
+
