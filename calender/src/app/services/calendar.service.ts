d3acbfd4b4fc6c06cbc767021069660b273997c0
Sherlock <playyala@yahoo.com>
2026-02-03 17:44:26 +0200

    add services of calendar

--- calender/src/app/services/calendar.service.ts
+++ calender/src/app/services/calendar.service.ts
@@ -4,6 +4,61 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class CalendarService {
+getDaysInMonth(year: number, month: number): Date[] {
+    const firstDayOfMonth = new Date(year, month, 1);
 
-  constructor() { }
+    const firstDayOffset =
+      firstDayOfMonth.getDay() === 0 ? 6 : firstDayOfMonth.getDay() - 1;
+
+    firstDayOfMonth.setDate(firstDayOfMonth.getDate() - firstDayOffset);
+
+    const totalDaysInMonth = new Date(year, month + 1, 0).getDate();
+    const days: Date[] = [];
+
+    for (let i = 0; i < totalDaysInMonth + firstDayOffset; i++) {
+      const day = new Date(firstDayOfMonth);
+      day.setDate(firstDayOfMonth.getDate() + i);
+      days.push(day);
+    }
+    return days;
+  }
+
+  nextMonth(
+    year: number,
+    month: number
+  ): { newYear: number; newMonth: number } {
+    if (month === 11) {
+      return { newYear: year + 1, newMonth: 0 };
+    } else {
+      return { newYear: year, newMonth: month + 1 };
+    }
+  }
+
+  previousMonth(
+    year: number,
+    month: number
+  ): { newYear: number; newMonth: number } {
+    if (month === 0) {
+      // go back to december of last year
+      return { newYear: year - 1, newMonth: 11 };
+    } else {
+      // previous month
+      return { newYear: year, newMonth: month - 1 };
+    }
+  }
+
+  isToday(date: Date): boolean {
+    const today = new Date();
+    return (
+      date.getDate() == today.getDate() &&
+      date.getMonth() === today.getMonth() &&
+      date.getFullYear() === today.getFullYear()
+    );
+  }
+
+  isPast(date: Date): boolean {
+    const today = new Date();
+    const todayMidnight = new Date(today.setHours(0, 0, 0, 0));
+    return date < todayMidnight;
+  }
 }
