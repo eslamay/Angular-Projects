a5b0779c836fec1c1e8015dad4b6f4a4b3179df2
Sherlock <playyala@yahoo.com>
2026-02-04 16:53:18 +0200

    add checker component

--- checkers/src/app/components/checker/checker.component.ts
+++ checkers/src/app/components/checker/checker.component.ts
@@ -1,11 +1,33 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { GameService } from '../../services/game.service';
 
 @Component({
   selector: 'app-checker',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './checker.component.html',
   styleUrl: './checker.component.scss'
 })
 export class CheckerComponent {
+constructor(public gameService: GameService) {}
 
+  onCellClick(row: number, col: number): void {
+    if (this.gameService.gameOver) return;
+    if (this.gameService.selectedPiece) {
+      this.gameService.movePiece(row, col);
+    } else {
+      this.gameService.selectPiece(row, col);
+    }
+  }
+
+  isSelected(rowIndex: number, colIndex: number): boolean {
+    return (
+      this.gameService.selectedPiece?.row === rowIndex &&
+      this.gameService.selectedPiece?.col === colIndex
+    );
+  }
+
+  resetGame(): void {
+    this.gameService.initializeBoard();
+  }
 }
