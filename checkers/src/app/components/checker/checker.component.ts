1ddbb7fbff78c788f30f7ed9454441b5e8beb8b8
Sherlock <playyala@yahoo.com>
2026-02-04 15:47:37 +0200

    add starter of checkers

---
+++ checkers/src/app/components/checker/checker.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-checker',
+  imports: [],
+  templateUrl: './checker.component.html',
+  styleUrl: './checker.component.scss'
+})
+export class CheckerComponent {
+
+}
