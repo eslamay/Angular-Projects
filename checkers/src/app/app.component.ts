1214da0c8fb57229aed07e84ac77f06733f648b6
Sherlock <playyala@yahoo.com>
2026-02-04 15:53:28 +0200

    clean starter of checkers

--- checkers/src/app/app.component.ts
+++ checkers/src/app/app.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { CheckerComponent } from "./components/checker/checker.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [CheckerComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
