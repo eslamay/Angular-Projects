51e4eb301563fbe9f1523e0bae209716377f53c3
Sherlock <playyala@yahoo.com>
2026-02-07 18:26:32 +0200

    add cellComponent of minesweeper

--- minesweeper/src/app/components/cell/cell.component.ts
+++ minesweeper/src/app/components/cell/cell.component.ts
@@ -1,11 +1,25 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-cell',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './cell.component.html',
   styleUrl: './cell.component.scss'
 })
 export class CellComponent {
+@Input() row!: number;
+@Input() col!: number;
+@Input() cell: any;
+@Output() cellClicked = new EventEmitter();
+@Output() cellFlagged = new EventEmitter();
 
+  onLeftClick() {
+    this.cellClicked.emit({ row: this.row, col: this.col });
+  }
+
+  onRightClick(event: MouseEvent) {
+    event.preventDefault();
+    this.cellFlagged.emit({ row: this.row, col: this.col });
+  }
 }
