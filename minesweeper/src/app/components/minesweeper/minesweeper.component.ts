62863304f37c2497ce93d515443d0c0fbeb8aeae
Sherlock <playyala@yahoo.com>
2026-02-07 18:32:10 +0200

    add mineComponent of minesweeper

--- minesweeper/src/app/components/minesweeper/minesweeper.component.ts
+++ minesweeper/src/app/components/minesweeper/minesweeper.component.ts
@@ -1,12 +1,29 @@
 import { Component } from '@angular/core';
 import { TimerComponent } from "../timer/timer.component";
+import { GameService } from '../../services/game.service';
+import { CommonModule } from '@angular/common';
+import { CellComponent } from "../cell/cell.component";
 
 @Component({
   selector: 'app-minesweeper',
-  imports: [TimerComponent],
+  imports: [TimerComponent, CommonModule, CellComponent],
   templateUrl: './minesweeper.component.html',
   styleUrl: './minesweeper.component.scss'
 })
 export class MinesweeperComponent {
+constructor(public gameService: GameService) {}
 
+  ngOnInit(): void {
+    this.gameService.initializeGame();
+  }
+
+  onCellClick(event: any) {
+    const { row, col } = event;
+    this.gameService.revealCell(row, col);
+  }
+
+  onCellFlagged(event: any) {
+    const { row, col } = event;
+    this.gameService.toggleFlag(row, col);
+  }
 }
