e0c42279aaae7c64a7754358f5889d3f29062f0d
Sherlock <playyala@yahoo.com>
2026-02-08 18:51:47 +0200

    add userService of advanced todo

--- advanced-todo/src/app/services/user.service.ts
+++ advanced-todo/src/app/services/user.service.ts
@@ -1,9 +1,29 @@
 import { Injectable } from '@angular/core';
+import { catchError, Observable, throwError } from 'rxjs';
+import { User } from '../models/user.model';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 
+const API_URL = 'https://jsonplaceholder.typicode.com/users';
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
+constructor(private http: HttpClient) {}
 
-  constructor() { }
+  fetchUsers(): Observable<User[]> {
+    return this.http.get<User[]>(API_URL).pipe(
+      catchError(this.handleError)
+    );
+  }
+
+  findUserById(userId: number): Observable<User> {
+    return this.http.get<User>(`${API_URL}/${userId}`).pipe(
+      catchError(this.handleError)
+    );
+  }
+
+  private handleError(error: HttpErrorResponse) {
+    console.error('HTTP Error:', error);
+    return throwError(() => new Error('Something went wrong'));
+  }
 }
