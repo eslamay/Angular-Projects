b278b6f28d3fa3be63165fe169944dfd7867f531
Sherlock <playyala@yahoo.com>
2026-02-10 18:54:42 +0200

    add resumeComponent of Resume-Builder

--- resume-builder/src/app/components/resume/resume.component.ts
+++ resume-builder/src/app/components/resume/resume.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { PdfGeneratorService } from '../../services/pdf-generator.service';
+import { ResumeDataService } from '../../services/resume-data.service';
+import { PhotoService } from '../../services/photo.service';
 
 @Component({
   selector: 'app-resume',
@@ -6,6 +10,50 @@ import { Component } from '@angular/core';
   templateUrl: './resume.component.html',
   styleUrl: './resume.component.scss'
 })
-export class ResumeComponent {
+export class ResumeComponent  implements AfterViewInit {
+  @ViewChild('resume') resumeRef!: ElementRef;
+  photoUrl: string | ArrayBuffer | null = null;
+  private photoUrlSubscription!: Subscription;
 
+  constructor(
+    public pdfService: PdfGeneratorService,
+    public resumeData: ResumeDataService,
+    public photoService: PhotoService
+  ) {}
+
+  ngAfterViewInit() {
+    this.pdfService.setResumeElement(this.resumeRef);
+    this.photoUrlSubscription = this.photoService.photoUrl$.subscribe((url) => {
+      this.photoUrl = url;
+    });
+  }
+
+  ngOnDestroy() {
+    if (this.photoUrlSubscription) {
+      this.photoUrlSubscription.unsubscribe();
+    }
+  }
+
+  isNotEmpty(str: string | null | undefined): boolean {
+    return str != null && str.trim() !== '';
+  }
+
+  hasNonEmptyItems(array: string[]): boolean {
+    return (
+      Array.isArray(array) && array.some((item) => item.trim().length >= 2)
+    );
+  }
+
+  hasNonEmptyProjects(
+    projects: { name: string; description: string }[]
+  ): boolean {
+    return (
+      Array.isArray(projects) &&
+      projects.some(
+        (project) =>
+          this.isNotEmpty(project.name) || this.isNotEmpty(project.description)
+      )
+    );
+  }
 }
+
