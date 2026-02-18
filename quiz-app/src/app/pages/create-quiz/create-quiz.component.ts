f49ff3fb4d0bcfa12b731ea1c939197e7e25001b
Sherlock <playyala@yahoo.com>
2026-02-18 18:32:22 +0200

    add starter of quiz app

---
+++ quiz-app/src/app/pages/create-quiz/create-quiz.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-create-quiz',
+  imports: [],
+  templateUrl: './create-quiz.component.html',
+  styleUrl: './create-quiz.component.scss'
+})
+export class CreateQuizComponent {
+
+}
