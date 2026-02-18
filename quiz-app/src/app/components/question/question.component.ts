86c9e65b1e0dfd2737bb11c3f7f779c6dc1b600a
Sherlock <playyala@yahoo.com>
2026-02-18 18:53:36 +0200

    add qurstionComponent of quiz app

--- quiz-app/src/app/components/question/question.component.ts
+++ quiz-app/src/app/components/question/question.component.ts
@@ -1,11 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Question } from '../../models/question.model';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-question',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './question.component.html',
   styleUrl: './question.component.scss'
 })
 export class QuestionComponent {
+@Input() question!: Question;
+@Output() answerSelected = new EventEmitter<boolean>();
 
+  selectOption(isCorrect: boolean) {
+    this.answerSelected.emit(isCorrect);
+  }
 }
