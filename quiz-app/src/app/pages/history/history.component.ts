19beb0beaddd570452a1377ccaf73e15a05ab9ed
Sherlock <playyala@yahoo.com>
2026-02-19 17:40:39 +0200

    add historyComponent of quiz app

--- quiz-app/src/app/pages/history/history.component.ts
+++ quiz-app/src/app/pages/history/history.component.ts
@@ -1,11 +1,21 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { CustomDatePipe } from '../../pipes/custom-date.pipe';
+import { HistoryService } from '../../services/history.service';
+import { History } from '../../models/history.model';
 
 @Component({
   selector: 'app-history',
-  imports: [],
+  imports: [CommonModule, CustomDatePipe],
   templateUrl: './history.component.html',
-  styleUrl: './history.component.scss'
+  styleUrl: './history.component.scss',
 })
-export class HistoryComponent {
+export class HistoryComponent implements OnInit {
+  history: History[] = [];
 
+  constructor(private historyService: HistoryService) {}
+
+  ngOnInit(): void {
+    this.history = this.historyService.getHistory();
+  }
 }
