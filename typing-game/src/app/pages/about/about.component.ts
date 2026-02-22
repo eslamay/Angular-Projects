3dbd60cbc0a71380009bcc84c1ba4eef195f0237
Sherlock <playyala@yahoo.com>
2026-02-23 01:00:48 +0200

    add aboutPage of typing-game

--- typing-game/src/app/pages/about/about.component.ts
+++ typing-game/src/app/pages/about/about.component.ts
@@ -1,11 +1,18 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AfterViewInit, Component } from '@angular/core';
 
 @Component({
   selector: 'app-about',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './about.component.html',
   styleUrl: './about.component.scss'
 })
-export class AboutComponent {
+export class AboutComponent implements AfterViewInit {
+  showContent: boolean = false;
 
+  ngAfterViewInit(): void {
+    setTimeout(() => {
+      this.showContent = true;
+    });
+  }
 }
