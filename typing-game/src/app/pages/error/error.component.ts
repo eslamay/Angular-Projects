014f23168e6ca9636e31231ae5b7eb23fd9e8340
Sherlock <playyala@yahoo.com>
2026-02-23 01:03:20 +0200

    add errorPage of typing-game

--- typing-game/src/app/pages/error/error.component.ts
+++ typing-game/src/app/pages/error/error.component.ts
@@ -1,11 +1,19 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AfterViewInit, Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-error',
-  imports: [],
+  imports: [RouterLink,CommonModule],
   templateUrl: './error.component.html',
   styleUrl: './error.component.scss'
 })
-export class ErrorComponent {
+export class ErrorComponent implements AfterViewInit {
+  showContent: boolean = false;
 
+  ngAfterViewInit(): void {
+    setTimeout(() => {
+      this.showContent = true;
+    }, 10);
+  }
 }
