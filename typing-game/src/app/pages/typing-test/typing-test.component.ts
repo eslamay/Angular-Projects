9a45fefc8f16b07e0a27f7fcdd49da27c6c693f1
Sherlock <playyala@yahoo.com>
2026-02-23 00:45:08 +0200

    add starter of typing-game

---
+++ typing-game/src/app/pages/typing-test/typing-test.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-typing-test',
+  imports: [],
+  templateUrl: './typing-test.component.html',
+  styleUrl: './typing-test.component.scss'
+})
+export class TypingTestComponent {
+
+}
