19da6e5d0786f802a99f5898917e2aadc8def054
Sherlock <playyala@yahoo.com>
2026-02-23 00:53:02 +0200

    add header of typing-game

--- typing-game/src/app/components/header/header.component.ts
+++ typing-game/src/app/components/header/header.component.ts
@@ -1,8 +1,10 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [CommonModule, RouterModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
