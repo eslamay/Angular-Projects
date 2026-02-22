54c0ca09121ee36a3bee3e54651fc8b114180c82
Sherlock <playyala@yahoo.com>
2026-02-23 00:58:00 +0200

    add homePage of typing-game

--- typing-game/src/app/pages/home/home.component.ts
+++ typing-game/src/app/pages/home/home.component.ts
@@ -1,11 +1,18 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AfterViewInit, Component } from '@angular/core';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
-export class HomeComponent {
+export class HomeComponent implements AfterViewInit {
+  showContent: boolean = false;
 
+  ngAfterViewInit(): void {
+    setTimeout(() => {
+      this.showContent = true;
+    });
+  }
 }
