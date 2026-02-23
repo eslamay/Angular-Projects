1bb976666d17819c7720db862cb3369c7f682895
Sherlock <playyala@yahoo.com>
2026-02-24 01:59:29 +0200

    add StatsComponent of typing-game

--- typing-game/src/app/pages/typing-test/stats/stats.component.ts
+++ typing-game/src/app/pages/typing-test/stats/stats.component.ts
@@ -1,11 +1,42 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
+import { Stats } from '../../../models/stats.model';
+import { StatsService } from '../../../services/game/stats.service';
 
 @Component({
   selector: 'app-stats',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './stats.component.html',
-  styleUrl: './stats.component.scss'
+  styleUrl: './stats.component.scss',
 })
-export class StatsComponent {
+export class StatsComponent implements OnInit, AfterViewInit {
+  showContent: boolean = false;
+  showStats = true;
+  stats: Stats = {
+    cleanSpeed: 0,
+    rawSpeed: 0,
+    accuracy: 0,
+    allWords: 0,
+    incorrectWords: 0,
+    allLetters: 0,
+    incorrectLetters: 0,
+  };
 
+  constructor(private statsService: StatsService) {}
+
+  ngOnInit(): void {
+    this.statsService.stats$.subscribe((newStats: Stats) => {
+      this.stats = newStats;
+    });
+  }
+
+  ngAfterViewInit(): void {
+    setTimeout(() => {
+      this.showContent = true;
+    }, 30);
+  }
+
+  toggleStats(): void {
+    this.showStats = !this.showStats;
+  }
 }
