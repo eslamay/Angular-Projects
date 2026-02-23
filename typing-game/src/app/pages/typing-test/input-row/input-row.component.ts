701606c6a10f761607f5c46996e3b01cd9707b22
Sherlock <playyala@yahoo.com>
2026-02-24 01:55:36 +0200

    add TypingTestComponent of typing-game

---
+++ typing-game/src/app/pages/typing-test/input-row/input-row.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-input-row',
+  imports: [],
+  templateUrl: './input-row.component.html',
+  styleUrl: './input-row.component.scss'
+})
+export class InputRowComponent {
+
+}
---
+++ typing-game/src/app/pages/typing-test/word-display/word-display.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-word-display',
+  imports: [],
+  templateUrl: './word-display.component.html',
+  styleUrl: './word-display.component.scss'
+})
+export class WordDisplayComponent {
+
+}
