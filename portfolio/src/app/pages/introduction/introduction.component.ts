0d72de4f6904ba0220e6d2780862df2dc77e6b53
Sherlock <playyala@yahoo.com>
2026-03-06 05:24:15 +0200

    add Introduction of portfolio

--- portfolio/src/app/pages/introduction/introduction.component.ts
+++ portfolio/src/app/pages/introduction/introduction.component.ts
@@ -1,11 +1,26 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { ButtonComponent } from '../../components/button/button.component';
 
 @Component({
   selector: 'app-introduction',
-  imports: [],
+  imports: [ButtonComponent],
   templateUrl: './introduction.component.html',
   styleUrl: './introduction.component.scss'
 })
 export class IntroductionComponent {
+greetingText: string = 'Hello! My name is';
+fullName: string = 'John Doe';
+roleText: string = 'I build solftware engineer';
+introductionText: string = `I'm a software developer focused on creating exceptional digital experiences. 
+                              Passionate about everything related to technology, I create user-friendly and efficient applications. 
+                              I love tackling complex problems and delivering high-quality work.`;
 
+aboutMeButtonText: string = 'About Me';
+
+  constructor(public router: Router) {}
+
+  navigate(): void {
+    this.router.navigate(['/about-me']);
+  }
 }
