7dbfbdced1cf504ab4f6193369b874e5eae1a42a
Sherlock <playyala@yahoo.com>
2026-03-12 10:27:46 +0200

    add services of portfolio

--- portfolio/src/app/pages/services/services.component.ts
+++ portfolio/src/app/pages/services/services.component.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { ButtonComponent } from "../../components/button/button.component";
 
 @Component({
   selector: 'app-services',
-  imports: [],
+  imports: [ButtonComponent],
   templateUrl: './services.component.html',
   styleUrl: './services.component.scss'
 })
 export class ServicesComponent {
+constructor(public router: Router) {}
 
+  navigate(): void {
+    this.router.navigate(['/contact']);
+  }
 }
